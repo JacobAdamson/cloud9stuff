bcf7ba19b225f7b6eb904400de3806d8917a3e47
jacobadamson <jadamson@colby.edu>
2016-01-07 15:22:15 +0000

    test commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
-
-admin.site.register(Question)
+from . import models
+#from .models import Question <- what the tutorial site says to do
+admin.site.register(models.Question)
 # Register your models here.
