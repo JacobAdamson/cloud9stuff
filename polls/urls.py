87ce6b6f9f862bb39f11633667c645f483d3df35
jacobadamson <jadamson@colby.edu>
2016-01-07 11:39:31 +0000

    namespaced URl name

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from  . import views
 
+app_name = 'polls' #needed for namespacing
 urlpatterns = [
         # ex: /polls"
         url(r'^$', views.index, name = 'index'),
+        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
         
         # ex: /polls/5/
         url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name = 'results'),
