cc9a1a5a2d3e274a1a86b35a629b3cf40e04c042
jacobadamson <jadamson@colby.edu>
2016-01-08 12:44:57 +0000

    Customized the admin change list

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return timezone.now() > self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
