244fce6bc355ca95bb0c237dbdee1aac4447b3a5
jacobadamson <jadamson@colby.edu>
2016-01-08 12:34:58 +0000

    changed to inline to conserve space on rendered page

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
     
 class QuestionAdmin(admin.ModelAdmin):
-     fieldsets = [
+    fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
